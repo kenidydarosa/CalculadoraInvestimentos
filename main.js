f9d6ac3e15edcf2e7c406bb5c3735d572c64ce56
Kenidy da Rosa <kenidyy@hotmail.com>
2024-01-23 21:13:52 -0300

    formulario completo

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 import { generateReturnArrays } from './investimentsGoals.js';
 
-const form = document.getElementById('investment-form'); 
+const form = document.getElementById('investment-form');
 // Poderia pegar o form e adicionar um evento de submit ou adicionar o "type"=submit no botão
 
 const calculateButton = document.getElementById('calculate-results');
+const clearFormButton = document.getElementById('clear-form');
 
 export function renderProgression(evento) {
   evento.preventDefault();
-//   const startingAmount = Number(
-    // document.getElementById('starting-amount').value
-//   );
-    const startingAmount = Number(form['starting-amount'].value) //Outra forma de acessar os elementos dentro de um form
+  //   const startingAmount = Number(
+  // document.getElementById('starting-amount').value
+  //   );
+
+  if (document.querySelector('.error')) {
+    return;
+  }
+  const startingAmount = Number(form['starting-amount'].value); //Outra forma de acessar os elementos dentro de um form
   const adicionalContribution = Number(
     document.getElementById('adition-contibution').value
   );
@@ -30,5 +35,52 @@ export function renderProgression(evento) {
   );
   console.log(returnsArray);
 }
-// form.addEventListener('submit', renderProgression);
+
+function clearForm() {
+  for (const element of form) {
+    if (element.tagName === 'INPUT' && element.hasAttribute('name')) {
+      element.value = '';
+    }
+    const errorInputContainers = document.querySelectorAll('.error');
+    for (const errorInputContainer of errorInputContainers) {
+      errorInputContainer.classList.remove('error');
+      errorInputContainer.parentElement.querySelector('p').remove();
+    }
+  }
+}
+function validateInput(event) {
+  if (event.target.value == '') {
+    return;
+  }
+
+  const { parentElement } = event.target;
+  const grandParentElement = event.target.parentElement.parentElement;
+  const inputValue = event.target.value.replace(',', '.');
+
+  const errorTextElement = document.createElement('p');
+
+  if (
+    isNaN(inputValue) ||
+    (Number(inputValue) <= 0 && !parentElement.classList.contains('error'))
+  ) {
+    errorTextElement.classList.add('text-red-500');
+    errorTextElement.innerText = 'Insira um valor númerico maior que 0!';
+    parentElement.classList.add('error');
+    grandParentElement.appendChild(errorTextElement);
+  } else if (
+    parentElement.classList.contains('error') &&
+    !isNaN(inputValue) &&
+    Number(inputValue) > 0
+  ) {
+    parentElement.classList.remove('error');
+    grandParentElement.querySelector('p').remove();
+  }
+}
+for (const formElement of form) {
+  if (formElement.tagName === 'INPUT' && formElement.hasAttribute('name')) {
+    formElement.addEventListener('blur', validateInput);
+  }
+}
+
 calculateButton.addEventListener('click', renderProgression);
+clearFormButton.addEventListener('click', clearForm);
