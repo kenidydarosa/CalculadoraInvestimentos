077f38deb64ecb078171870bff3837d2f970f7de
Kenidy da Rosa <kenidyy@hotmail.com>
2024-01-28 11:23:09 -0300

    Inclusão de arquivos

---
+++ src/investimentsGoals.js
@@ -0,0 +1,54 @@
+function convertToMonthlyReturnRate(yearReturnRate) {
+  return yearReturnRate ** (1 / 12);
+}
+
+export function generateReturnArrays(
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = 'monthly',
+  monthlyContribution = 0,
+  returnRate = 0,
+  returTimeFrame = 'monthly'
+) {
+  if (!timeHorizon || !startingAmount) {
+    {
+      throw new Error(
+        'Investimento inicial e prazo devem ser preenchidos com valores positivos.'
+      );
+    }
+  }
+  const finalReturnRate = returTimeFrame === 'Monthly' ? 1 + returnRate / 100
+                        : convertToMonthlyReturnRate(1 + returnRate / 100);
+
+  const finalTimeHorizon =
+    timePeriod === 'monthly' ? timeHorizon : timeHorizon * 12;
+
+  const referenceInvestimentObject = {
+    investedAmount: startingAmount,
+    interestReturns: 0,
+    totalInterestReturns: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
+  const returnArray = [referenceInvestimentObject];
+
+  for (
+    let timeReference = 1;
+    timeReference <= finalTimeHorizon;
+    timeReference++
+  ) {
+    const totalAmount = returnArray[timeReference - 1].totalAmount * finalReturnRate + monthlyContribution;
+    const interestReturns = returnArray[timeReference - 1].totalAmount * (finalReturnRate -1);
+    const investedAmount = startingAmount + monthlyContribution * timeReference;
+    const totalInterestReturns = totalAmount - investedAmount;
+
+    returnArray.push({
+      investedAmount,
+      interestReturns,
+      totalInterestReturns,
+      month: timeReference,
+      totalAmount,
+    });
+  }
+  return returnArray;
+}
---
+++ src/table.js
@@ -0,0 +1,66 @@
+const isNonEmptyArray = (arrayElement) => {
+  return Array.isArray(arrayElement) && arrayElement.lenght > 0;
+};
+
+export const createTable = (columnsArray, dataArray, tableId) => {
+  if (
+    !isNonEmptyArray(columnsArray) &&
+    !isNonEmptyArray(dataArray) &&
+    !tableId
+  ) {
+    throw new Error(
+      'Para a correta execução, precisamos de um array com as colunas, outro com as informações das linhas e também o id do elemento tabela selecionado'
+    );
+  }
+  const tableElement = document.getElementById('results-table');
+
+  if (!tableElement && tableElement.nodeName !== 'TABLE') {
+    throw new Error('Id informado não corresponde a nenhum elemento table');
+  }
+  createTableHeader(tableElement, columnsArray);
+  createTableBody(tableElement, dataArray, columnsArray);
+};
+
+function createTableHeader(tableReference, columnsArray) {
+  //Cria um cabeçalho caso a tabela ainda não tenha
+  function createTheadElement(tableReference) {
+    const thead = document.createElement('thead');
+    tableReference.appendChild(thead);
+    return thead;
+  }
+  //Verifica se a tabela contém um cabeçalho, caso não tenha chama a função de criação
+  const tableHeaderReference =
+    tableReference.querySelector('thead') ?? createTheadElement(tableReference);
+
+  const headerRow = document.createElement('tr');
+  ['bg-gray-700','text-slate-200','sticky','top-0'].forEach(cssClass => headerRow.classList.add(cssClass))
+  for (const tableColumnObject of columnsArray) {
+    const headerElement = /*html*/ `<th class= 'text-center'> ${tableColumnObject.columnLabel}</th>`;
+    headerRow.innerHTML += headerElement;
+  }
+  tableHeaderReference.appendChild(headerRow);
+}
+
+function createTableBody(tableReference, tableItens, columnsArray) {
+  function createTbodyElement(tableReference) {
+    const tbody = document.createElement('tbody');
+    tableReference.appendChild(tbody);
+    return tbody;
+  }
+  const tableBodyReference =
+    tableReference.querySelector('tbody') ?? createTbodyElement(tableReference);
+
+  for (const [itemIndex, tableItem] of tableItens.entries()) {
+    const tableRow = document.createElement('tr');
+    if(itemIndex % 2 !== 0 ){
+      tableRow.classList.add('bg-gray-200')
+    }
+    for (const tableColumn of columnsArray) {
+      const formarFn = tableColumn.format ?? ((info) => info);
+      tableRow.innerHTML += /*html*/ `<td class ="text-center">${
+        formarFn(tableItem[tableColumn.accessor])
+      }</td>`;
+    }
+    tableBodyReference.appendChild(tableRow);
+  }
+}
